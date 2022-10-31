56915a00002a8ad6162a30fbcdc3ebfe95c81904
Chris Fairless <chrisfairless@hotmail.com>
2022-10-31 01:10:58 +0100

    Fixup staticfiles settings

--- climada_calc/settings.py
+++ climada_calc/settings.py
@@ -130,9 +130,7 @@ USE_TZ = True
 
 STATIC_ROOT = BASE_DIR / "static"
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
+STATICFILES_DIRS = []
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_ROOT = BASE_DIR / "media"
